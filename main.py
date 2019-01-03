54203246b1b715c78fd2721c2e6da24f8d6d84d0
Netlight <jasoness@yandex.ru>
2019-01-03 23:44:41 +0300

    Fix logic, add requierements, pipfile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 from telebot import apihelper
+from telebot import types
 
 
 import settings
@@ -13,12 +14,33 @@ bot = telebot.TeleBot(settings.TOKEN)
 def send_welcome(message):
     bot.send_message(
         message.from_user.id,
-        '*This is a simple Bookmarks bot.*' +
-        '\n\n' +
-        '_Let\'s start using!_' +
-        '\n' +
-        'Just simply send any type of file (including text) and I save it to you personal bookmarks, which you can use anywhere you are.',
+        '''*This is a simple Bookmarks bot.*
+
+        _Let\'s start using!_
+        Just simply send any type of file (including text) and I\'ll save it to your personal bookmarks, that you can use wherever you are.''',
         parse_mode = 'Markdown')
 
-    bot.send_photo(message.from_user.id, 'AgADAgADxakxG3J-cUmyIQrGeEoOrG5MOQ8ABD7pse51WVJeG0YBAAEC')
+    @bot.message_handler(commands=['git'])
+    def send_git(message):
+        bot.send_message(
+        message.from_user.id,
+        '[My git REPO](https://github.com/Netlighter/Bookmarks_Bot)',
+        parse_mode = 'Markdown')
+
+
+    @bot.message_handler(content_types=[
+    'text',
+    'photo',
+    'document',
+    'audio',
+    'voice',
+    'video_note',])
+    def add_bookmark(message):
+        #broken code...
+
+        if message.content == 'text'
+
+        bot.send_photo(message.from_user.id, message.photo[-1].file_id)
+
+
 bot.polling()
